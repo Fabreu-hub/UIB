c7483db70b61ee5b77a74311c6663330202988fc
Filipe Abreu Andrade Silva <1192306675@acd.grupotiradentes.com>
2020-03-02 22:05:22 -0300

    foram colocado os Get e Sets das classes conta e cliente

--- src/Conta.java
+++ src/Conta.java
@@ -5,4 +5,21 @@ public class Conta {
 	public Double saldoConta;
 	public Cliente donoDaConta;
 	
+	public String getnumeroConta() {
+		return numeroConta;
+	}
+	public void setnumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	public Double getnsaldoConta() {
+		return saldoConta;
+	}
+	public void setsaldoConta(Double saldoConta) {
+		this.saldoConta = saldoConta;
+	}public Cliente getndonoDaConta() {
+		return donoDaConta;
+	}
+	public void setdonoDaConta(Cliente donoDaConta) {
+		this.donoDaConta = donoDaConta;
+	}
 }
