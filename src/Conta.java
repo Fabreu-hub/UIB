9092db2c47198e0aa6fa29437858be3ee010e036
Filipe Abreu Andrade Silva <1192306675@acd.grupotiradentes.com>
2020-03-09 21:52:12 -0300

    Atualizacao do sistema do Internet Banking

--- src/Conta.java
+++ src/Conta.java
@@ -4,52 +4,51 @@ public class Conta {
 	private String numeroConta;
 	private Double saldoConta;
 	private Cliente cliente;
-	public Double creditar;
-	public Double debitar;
-	public Double transferir;
-	
-	
-	
-	
-	
+
 	public String getnumeroConta() {
 		return numeroConta;
 	}
+
 	public void setnumeroConta(String numeroConta) {
 		this.numeroConta = numeroConta;
 	}
+
 	public Double getnsaldoConta() {
 		return saldoConta;
 	}
+
 	public void setsaldoConta(Double saldoConta) {
 		this.saldoConta = saldoConta;
-	}public Cliente getcliente() {
+	}
+
+	public Cliente getcliente() {
 		return cliente;
 	}
+
 	public void setcliente(Cliente donoDaConta) {
 		this.cliente = donoDaConta;
 	}
-	
-	public Double getCreditar() {
-		return creditar;
+
+	public void creditar(double valor) {
+
+		saldoConta += valor;
+
 	}
-	public void setCreditar(Double creditar) {
-		this.creditar = creditar;
+
+	public void debitar(double valor) {
+
+		saldoConta -= valor;
+
 	}
-	public Double getDebitar() {
-		return debitar;
+
+	//método de transferencia entre contas
+
+	public void transferir(Conta contaDestino , Double valor) {
+		
+		debitar(valor);
+		
+		contaDestino.creditar(valor);
+		
 	}
-	public void setDebitar(Double debitar) {
-		this.debitar = debitar;
-	}
-	public Double getTransferir() {
-		return transferir;
-	}
-	public void setTransferir(Double transferir) {
-		this.transferir = transferir;
-	}
-	
-	
-	
-	
+
 }
--- src/UIBMain.java
+++ src/UIBMain.java
@@ -1,28 +1,55 @@
+import java.util.Scanner;
+
 
 public class UIBMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Cliente clientes = new Cliente();
-		Conta conta = new Conta();
-		
-		clientes.cpf = "09574041409";
-		clientes.nomeCliente = "filipe abreu";
-		clientes.contaCliente= conta;
+		Scanner leTeclado = new Scanner(System.in);
+		final int TOTAL_CLIENTE = 2 ;
 		
 		
-		conta.numeroConta = "224786";
-		conta.saldoConta = 4000.0;
-		conta.donoDaConta = clientes;
 		
-		System.out.println(clientes.cpf);
-		System.out.println(clientes.nomeCliente);
+		System.out.println("Bem vindo ao Unit Internet Banking ");
+		System.out.println("------------------------------------ ");
 		
-		System.out.println(conta.numeroConta);
-		System.out.println(conta.saldoConta);
+		Cliente clientes[] = new Cliente[TOTAL_CLIENTE];
 		
-		System.out.println(conta.donoDaConta.nomeCliente + " tem " + conta.saldoConta);
-		System.out.println(clientes.nomeCliente + "  tem " + clientes.contaCliente.saldoConta);
+		Conta contas[] = new Conta[TOTAL_CLIENTE];
+		
+		
+		for (int i = 0; i < TOTAL_CLIENTE; i++) {
+			clientes[i] = new Cliente();
+					
+			System.out.println("Digite o Nome do Cliente");
+			clientes[i].setNomeCliente(leTeclado.next());
+			System.out.println("Digite o cpf do Cliente");
+			clientes[i].setCpf(leTeclado.next());
+			
+		}
+		
+		for (int i = 0; i < TOTAL_CLIENTE; i++) {
+			contas[i] = new Conta();
+					
+			System.out.println("Digite o Numero da conta ");
+			contas[i].setnumeroConta(leTeclado.next());
+			
+			System.out.println("Digite o saldo do Cliente");
+			contas[i].setsaldoConta(leTeclado.nextDouble());
+			
+			contas[i].setcliente(clientes[i]);
+			
+		}
+		
+		contas[0].debitar(100);
+		contas[0].creditar(20);
+		contas[1].transferir(contas[0], 50.0);
+		
+		for (int j = 0; j < TOTAL_CLIENTE; j++) {
+			System.out.println("Saldo da conta " + contas[j].getnumeroConta() + " é " + contas[j].getnsaldoConta() );
+		}
+		
+		leTeclado.close();
+		
 	}
-
 }
